b8572499b06e8a2078f5dd388546fea139e08bf2
Armin Hupka <arminhupka@me.com>
2023-05-18 09:55:43 +0200

    - added regulations page

---
+++ pages/regulamin/index.tsx
@@ -0,0 +1,289 @@
+import { ReactElement } from "react";
+
+import SectionTitle from "../../components/atoms/SectionTitle/SectionTitle";
+import MainLayout from "../../components/layouts/MainLayout";
+
+const Test = (): ReactElement => (
+  <MainLayout>
+    <SectionTitle title='Regulamin' />
+    <h2>§1 Postanowienia Ogólne</h2>
+    <ol>
+      <li>
+        Niniejszy regulamin reguluje zasady korzystania ze Sklepu Internetowego
+        OLGA WAŁEK FOTOGRAFIA NIP 899-287-92-63 prowadzonego pod adresem
+        https://warsztaty.olgawalek.pl/.
+      </li>
+      <li>
+        Sklep internetowy, działający pod adresem
+        https://warsztaty.olgawalek.pl/ (zwany dalej: Sklep internetowy
+        warsztaty.olgawalek.pl), jest prowadzony przez: OLGA WAŁEK FOTOGRAFIA,
+        Szkolna 10, 98-432 Dzietrzkowice NIP 899-287-92-63.
+      </li>
+      <li>
+        Dane teleadresowe przedsiębiorcy, umożliwiające kupującemu kontakt ze
+        sprzedawcą: 1) adres pocztowy: Szkolna 10, 98-432 Dzietrzkowice. 2)
+        adres e-mail: olga.walek1@gmail.com.
+        <ol>
+          <li>adres pocztowy: Szkolna 10, 98-432 Dzietrzkowice. </li>
+          <li>adres e-mail: olga.walek1@gmail.com. </li>
+        </ol>
+      </li>
+      <li>
+        Regulamin sklepu internetowego warsztaty.olgawalek.pl umieszczony jest
+        nieprzerwanie na stronie internetowej https://beautyphoto-academy.eu/ w
+        sposób umożliwiający Klientom jego pozyskanie, odtwarzanie i utrwalanie
+        jego treści. Regulamin jest również udostępniany Klientom przed
+        zawarciem umowy. 
+      </li>
+    </ol>
+    <h2>§2 Definicje</h2>
+    <ol>
+      <li>
+        Konsument – jest nią osoba fizyczna w rozumieniu art. 221 Kodeksu
+        cywilnego. 
+      </li>
+      <li>
+        Przedsiębiorca na prawach konsumenta – jest nią osoba fizyczna
+        zawierająca umowę bezpośrednio związaną z jej działalnością gospodarczą,
+        gdy z treści tej umowy wynika, że nie posiada ona dla tej osoby
+        charakteru zawodowego, wynikającego w szczególności z przedmiotu
+        wykonywanej przez nią działalności gospodarczej, udostępnionego na
+        podstawie przepisów o Centralnej Ewidencji i Informacji o Działalności
+        Gospodarczej.
+      </li>
+      <li>
+        Przedsiębiorca – oznacza osobę fizyczną, osobę prawną lub jednostkę
+        organizacyjną niebędącą osobą prawną, której ustawa przyznaje zdolność
+        prawną, prowadzącą we własnym imieniu działalność gospodarczą lub
+        zawodową i dokonującą czynności prawnej związanej bezpośrednio z jej
+        działalnością gospodarczą lub zawodową.
+      </li>
+      <li>
+        Usługodawca/Sprzedawca – OLGA WAŁEK FOTOGRAFIA, Szkolna 10, 98-432
+        Dzietrzkowice.
+      </li>
+      <li>
+        Sklep internetowy – sklep internetowy prowadzony pod adresem
+        https://beautyphoto-academy.eu/.
+      </li>
+      <li>
+        Kodeks cywilny – ustawa z dnia 23 Kwietnia 1964r. (Dz.U.2020.1740).
+      </li>
+      <li>
+        Towary oraz Usługi – wszelkie usługi świadczone oraz towary będące
+        przedmiotem umowy sprzedaży między sprzedawcą a kupującym.
+      </li>
+      <li>
+        Regulamin – niniejszy regulamin świadczenia usług drogą elektroniczną w
+        rozumieniu art. 8 ustawy o świadczeniu usług drogą elektroniczną z dnia
+        18 lipca 2002 r. (Dz.U.2020.344).
+      </li>
+      <li>
+        Klient/Kupujący – osoba fizyczna, osoba prawna lub jednostka
+        organizacyjna niebędąca osobą prawną, której przepisy szczególne
+        przyznają zdolność prawną, która dokonuje lub planuje dokonać Zamówienia
+        w ramach Sklepu Internetowego z wykorzystaniem drogi elektronicznej, w
+        tym również Konsumenta, Przedsiębiorcę na prawach konsumenta oraz
+        Przedsiębiorcę.
+      </li>
+      <li>
+        Zamówienie – oświadczenie woli Klienta, w którym to Klient akceptuje
+        ofertę sprzedaży Sprzedawcy, która zawiera w szczególności cenę, rodzaj,
+        ilość Towarów lub Usług w ramach umowy sprzedaży za pośrednictwem sklepu
+        internetowego.
+      </li>
+      <li>
+        Ustawa o prawach konsumenta – ustawa z dnia 30 maja 2014 r. o prawach
+        konsumenta (Dz.U.2020.287).
+      </li>
+    </ol>
+    <h2>§2 Zasady korzystania ze sklepu internetowego</h2>
+    <ol>
+      <li>
+        Ceny wszystkich oferowanych przez Sklep towarów i usług są cenami brutto
+        (zawierają podatek VAT) oraz są wyrażone w złotych polskich. Odstępstwa
+        od zasady waluty polskiej są wyraźnie zaznaczone.
+      </li>
+      <li>
+        Ceny widniejące na stronie internetowej sklepu
+        https://beautyphoto-academy.eu/, jak również opisy towarów i usług
+        stanowią jedynie informację handlową, a nie ofertę w rozumieniu Kodeksu
+        Cywilnego. Charakter wiążący – na potrzeby zawarcia konkretnej umowy –
+        zyskują one dopiero z chwilą potwierdzenia przyjęcia zamówienia do
+        realizacji przez Sprzedającego.
+      </li>
+      <li>
+        Sprzedawca używa w Sklepie internetowym https://beautyphoto-academy.eu/
+        kodów rabatowych upoważniających do zakupów towarów z rabatem
+        zamieszczonym na kuponie. Kody rabatowe nie podlegają zamianie na
+        równowartość pieniężną.
+      </li>
+      <li>
+        Do złożenia zamówienia w sklepie internetowym
+        https://beautyphoto-academy.eu/ konieczna jest akceptacja niniejszego
+        Regulaminu.
+      </li>
+      <li>
+        Do skutecznego złożenia zamówienia w Sklepie internetowym
+        https://beautyphoto-academy.eu/ konieczne jest posiadanie urządzenia z
+        dostępem do Internetu umożliwiające przeglądanie stron internetowych, a
+        także klawiatura lub inne urządzenie umożliwiające poprawne wypełnienie
+        formularzy elektronicznych.
+      </li>
+      <li>
+        Strona Sklepu internetowego https://beautyphoto-academy.eu/ posiada
+        certyfikat SSL – bezpieczny protokół szyfrowania komunikacji.
+      </li>
+    </ol>
+    <h2>§4 Warunki zawierania umowy sprzedaży</h2>
+    <ol>
+      <li>
+        Zamówienia w sklepie internetowym https://beautyphoto-academy.eu/ można
+        dokonywać poprzez wypełnienie odpowiednich formularzy dostępnych na
+        stronach sklepu.
+      </li>
+      <li>
+        Klient podczas składania zamówienia jest zobowiązany do podania
+        prawidłowych danych osobowych: imienia i nazwiska, adresu zamieszkania,
+        adresu e-mail oraz telefonu komórkowego.
+      </li>
+      <li>
+        Po otrzymaniu zamówienia, Sprzedawca potwierdza jego otrzymanie oraz
+        jednocześnie informuje o przyjęciu Zamówienia do realizacji.
+        Potwierdzenie zamówienie odbywa się poprzez przesłanie przez
+        Sprzedającego do Klienta odpowiedniej wiadomości mailowej na adres
+        podany w formularzu zamówienia. Z chwilą potwierdzenia przyjęcia
+        zamówienia do realizacji przez Sprzedającego, umowa uważana jest za
+        zawartą.
+      </li>
+      <li>
+        Udostępnienie oraz utrwalenie treści umowy sprzedaży następuje poprzez
+        udostępnienie niniejszego Regulaminu na stronie Sklepu Internetowego,
+        przesłanie stosownej informacji na adres mailowy Klienta podany przy
+        składaniu zamówienia, o którym mowa w §4 pkt. 4.2.
+      </li>
+      <li>
+        Dokonywanie Zamówień w Sklepie internetowym
+        https://beautyphoto-academy.eu/ jest możliwe 24 godziny na dobę przez
+        wszystkie dni w roku. 
+      </li>
+    </ol>
+    <h2>§5 Sposób płatności i termin płatności</h2>
+    <ol>
+      <li>
+        W sklepie https://beautyphoto-academy.eu/ istnieje możliwość płatności w
+        jeden z następujących sposobów: – przelew na konto bankowe Sprzedawcy, –
+        za pośrednictwem usług płatniczych dostawców zewnętrznych.
+      </li>
+      <li>
+        Po złożeniu zamówienia, Klient otrzymuje drogą poczty elektronicznej
+        (e-mail) numer rachunku bankowego, na który należy przelać właściwą
+        kwotę. Z chwilą zaksięgowania wpłaty na rachunku bankowym sklepu
+        https://beautyphoto-academy.eu/ zamówienie jest przekazywane do
+        realizacji.
+      </li>
+      <li>
+        Sklep internetowy https://beautyphoto-academy.eu/ umożliwia wpłatę
+        należności za pośrednictwem zewnętrznego serwisu płatności
+        elektronicznych: PayU.
+      </li>
+      <li>
+        Klient jest obowiązany do dokonania płatności za pomocą jednego z wyżej
+        wymienionych sposobów w terminie 7 dni kalendarzowych od dnia zawarcia
+        umowy sprzedaży.
+      </li>
+    </ol>
+    <h2>§6 Dostawa</h2>
+    <ol>
+      <li>
+        Zamówione produkty elektroniczne są dostarczane na adres e-mail, podany
+        w zamówieniu.
+      </li>
+      <li>
+        Czas wysyłki produktów https://beautyphoto-academy.eu/ wynosi 48 godzin.
+        Sprzedawca dołoży wszelkich starań, aby dostawa produktu nastąpiła w
+        czasie podanym w karcie produktu w sklepie internetowym. W przypadku
+        niemożliwości dostawy produktu w wyżej wymienionym czasie, Kupujący
+        zostanie o tym niezwłocznie poinformowany.
+      </li>
+    </ol>
+    <h2>§7 Odstąpienie do umowy</h2>
+    <ol>
+      <li>
+        Kupujący będący Konsumentem lub Przedsiębiorcą na prawach konsumenta,
+        który zawarł umowę na odległość lub poza lokalem przedsiębiorstwa, ma
+        prawo odstąpić od zawartej umowy sprzedaży w terminie 14 dni od daty
+        wydania mu produktu bez podania przyczyny.
+      </li>
+      <li>
+        W przypadku skorzystania z prawa, o którym mowa w punkcie
+        poprzedzającym, Kupujący powinien poinformować o tym Sprzedającego,
+        składając oświadczenie o odstąpieniu, które może zostać wysłane pocztą
+        na adres sklepu, może to także zrobić drogą elektroniczną za pomocą
+        elektronicznego formularza odstąpienia od umowy przesłanego na adres
+        mailowy sprzedawcy olga.walek1@gmail.com. Do zachowania terminu
+        wystarczy wysłanie oświadczenia przed jego upływem. Oświadczenie o
+        odstąpieniu złożone po terminie, o którym mowa w §7 pkt. 7.1 nie
+        wywołuje skutków prawnych.
+      </li>
+      <li>
+        W przypadku złożenia przez Konsumenta lub Przedsiębiorcę na prawach
+        konsumenta, oświadczenia o odstąpieniu drogą elektroniczną za
+        pośrednictwem elektronicznego formularza odstąpienia od umowy,
+        Sprzedawca niezwłocznie prześle potwierdzenie otrzymania oświadczenia o
+        odstąpieniu od umowy.
+      </li>
+      <li>
+        W przypadku odstąpienia od umowy wszystkie dokonane przez Kupującego
+        płatności, w tym koszty dostarczenia, zostaną przekazane na rachunek
+        bankowy Kupującego niezwłocznie, nie później jednak niż w terminie 14
+        dni od dnia otrzymania przez Sprzedającego oświadczenia Kupującego o
+        odstąpieniu od umowy.
+      </li>
+      <li>
+        Sprzedawca oświadcza, iż zwrot płatności, o którym mowa w § 7 pkt 7.9
+        Regulaminu zostanie zrealizowany przy użyciu takiego samego sposobu
+        zapłaty, jakiego użył Kupujący. Sprzedawca w porozumieniu z Kupującym,
+        może ustalić inny sposób zwrotu, który nie będzie wiązał się z
+        koniecznością poniesienia przez Kupującego dodatkowych kosztów.
+      </li>
+      <li>
+        Sprzedawca oświadcza, że prawo do odstąpienia nie przysługuje w
+        odniesieniu do umów ujętych w art. 38 ustawy z 30.5.2014 r. – o prawach
+        konsumenta.
+      </li>
+      <li>
+        Zawierając umowę Konsument lub Przedsiębiorca na prawach konsumenta
+        potwierdza, iż zapoznał się z treścią niniejszego regulaminu, w tym
+        potwierdza, iż został zawiadomiony o prawie odstąpienia od zawartej
+        umowy sprzedaży w terminie 14 dni od daty wydania mu produktu.
+      </li>
+    </ol>
+    <h2>§8 Reklamacje</h2>
+    <ol>
+      <li>
+        Podstawa i zakres odpowiedzialności Sprzedawcy wobec Klienta, z tytułu
+        niezgodności Produktu z Umową są określone powszechnie obowiązującymi
+        przepisami prawa oraz poniższymi postanowieniami Regulaminu.
+      </li>
+      <li>Sprzedawca jest obowiązany dostarczyć produkt bez wad.</li>
+      <li>
+        W przypadku stwierdzenia uszkodzeń mechanicznych, powstałych podczas
+        dostawy lub w przypadku skierowania reklamacji do Sprzedającego,
+        Kupujący powinien przesłać informacje o tym fakcie listownie lub poprzez
+        e-mail na adres. W zgłoszeniu zaleca się wskazać wadę, jaką zdaniem
+        Klienta towar posiada, oraz jeżeli jest to możliwe – udokumentować
+        rzeczoną wadę, datę wystąpienia wady, żądanie sposobu doprowadzenia do
+        zgodności towaru z umową zgodnie z art. 560 i nast. Kodeksu Cywilnego
+        oraz podanie danych kontaktowych.
+      </li>
+      <li>
+        Sprzedawca w terminie 14 dni od otrzymania zgłoszenia ustosunkuje się do
+        zgłoszenia. Brak ustosunkowania się Sprzedawcy w powyższym terminie
+        oznacza, że Sprzedawca uznał reklamację za uzasadnioną.
+      </li>
+    </ol>
+  </MainLayout>
+);
+
+export default Test;
