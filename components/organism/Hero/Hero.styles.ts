7e9c16285102f691dcd775ed1de94d9a5b4a0c8b
Armin Hupka <arminhupka@me.com>
2023-05-21 01:01:58 +0200

    - changed hero size

--- components/organism/Hero/Hero.styles.ts
+++ components/organism/Hero/Hero.styles.ts
@@ -1,11 +1,8 @@
 import { Box, styled } from "@mui/material";
 
 export const StyledWrapper = styled(Box)(({ theme }) => ({
-  height: 350,
   background: theme.palette.primary.main,
   overflow: "hidden",
   borderRadius: theme.spacing(1),
-  [theme.breakpoints.up("md")]: {
-    height: 550,
-  },
+  aspectRatio: "16/7",
 }));
