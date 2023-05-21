1a22b78e4abeaa15bff0ffda271547926ae0c22f
Armin Hupka <arminhupka@me.com>
2023-05-21 13:18:11 +0200

    - created activation page

---
+++ pages/konto/aktywacja/[token].tsx
@@ -0,0 +1,53 @@
+import { Alert, Box } from "@mui/material";
+import { NextPage, NextPageContext } from "next";
+
+import { OkResponseDto } from "../../../api/api-types";
+import { client } from "../../../api/client";
+import SectionTitle from "../../../components/atoms/SectionTitle/SectionTitle";
+import MainLayout from "../../../components/layouts/MainLayout";
+
+interface INextPageProps {
+  activated: boolean;
+}
+
+const ActivationPage: NextPage<INextPageProps> = ({ activated }) => {
+  return (
+    <MainLayout>
+      <SectionTitle title='Aktywacja konta' />
+      <Box mt={2}>
+        {activated && (
+          <Alert severity='success'>
+            Twoje konto zostało aktywowane! Możesz się zalogować.
+          </Alert>
+        )}
+        {!activated && (
+          <Alert severity='error'>Wyglada na to że ten link już wygasł.</Alert>
+        )}
+      </Box>
+    </MainLayout>
+  );
+};
+
+export const getServerSideProps = async (ctx: NextPageContext) => {
+  const { token } = ctx.query;
+
+  let isActivated = false;
+
+  try {
+    const { data } = await client.get<OkResponseDto>(
+      `/users/activate/${token}`,
+    );
+    isActivated = true;
+  } catch (e) {
+    console.error(e);
+    isActivated = false;
+  }
+
+  return {
+    props: {
+      activated: isActivated,
+    },
+  };
+};
+
+export default ActivationPage;
